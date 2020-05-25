32ea573da2819c1128a967a8e7157c5083db754a
Alexandr <sasha.dubovchenko@gmail.com>
2020-05-25 13:16:15 +0300

    #2 e2e tests add

--- e2e/appium.test.js
+++ e2e/appium.test.js
@@ -13,7 +13,7 @@ const driver = wd.promiseChainRemote('localhost', PORT);
 
 beforeAll(async () => {
   await driver.init(config);
-  await driver.sleep(2000); // wait for app to load
+  await driver.sleep(4000); // wait for app to load
 })
 
 test('appium renders', async () => {
